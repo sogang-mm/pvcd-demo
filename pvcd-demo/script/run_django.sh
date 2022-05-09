1ace3df3057b2c6bfc2c90d6bb73482df24e6fb6
ms-jeong <jms8167@gmail.com>
2022-05-09 16:31:18 +0900

    change default port

--- pvcd-demo/script/run_django.sh
+++ pvcd-demo/script/run_django.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -x
 ip="${1:-0}"
-port="${2:-8001}"
-python /workspace/manage.py runserver $ip:$port
+port="${2:-8080}"
+python manage.py runserver $ip:$port
--- pvcd-demo/script/run_gunicorn.sh
+++ pvcd-demo/script/run_gunicorn.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 set -x
 ip="${1:-0}"
-port="${2:-8777}"
-
+port="${2:-8080}"
 gunicorn pvcd_demo.wsgi --bind $ip:$port
 
